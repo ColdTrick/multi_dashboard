6e7bd466985e3f8dd6821be282b7d1c728951522
BenaeSan <benoitmottin@ymail.com>
2017-04-07 14:48:14 +0200

    chore: added french translation

---
+++ languages/fr.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+	'item:object:multi_dashboard' => "Multi Dashboard",
+
+	'multi_dashboard:new' => "Ajouter un nouveau tableau de bord",
+	'multi_dashboard:edit' => "Modifier le tableau de bord: %s",
+
+	'multi_dashboard:types:title' => "Sélectionnez le type de tableau de bord",
+	'multi_dashboard:types:widgets' => "Widgets",
+	'multi_dashboard:types:iframe' => "iFrame",
+
+	'multi_dashboard:num_columns:title' => "Nombre de colonnes",
+	'multi_dashboard:iframe_url:title' => "iFrame URL",
+	'multi_dashboard:iframe_url:description' => "Note: veuillez vérifier que l'URL commence par http:// ou https://. Tous les sites ne supportent pas de l'utilisation d'iFrame",
+	'multi_dashboard:iframe_height:title' => "Hauteur d'iFrame",
+
+	// multi dashboard - edit
+	'multi_dashboard:actions:edit:error:input' => "Entrée invalide, veuillez mettre un titre",
+	'multi_dashboard:actions:edit:success' => "Tableau de bord créé/modifier avec succès",
+
+	// multi dashboard - delete
+	'multi_dashboard:actions:delete:error:delete' => "Impossible de supprimer le tableau de bord %s",
+	'multi_dashboard:actions:delete:success' => "Le tableau de bord %s a été supprimer avec succès",
+
+	// multi dashboard - drop
+	'multi_dashboard:actions:drop:success' => "Le widget à bien été déplacé vers le nouveau tableau de bord",
+
+	// reorder
+	'multi_dashboard:actions:reorder:error:order' => "Veuillez fournir un nouvel ordre",
+	'multi_dashboard:actions:reorder:success' => "Tableau de bord réorganisé avec succès",
+	
+	];
